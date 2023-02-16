b1fc4fcdab28c75b3728d6cc1572514c501ef4ac
datboiluiskrrt <datboiluiskrrt@gmail.com>
2023-02-15 19:48:49 -0600

    Created put endpoint for users.

--- WebApplication1/Controllers/UsersController.cs
+++ WebApplication1/Controllers/UsersController.cs
@@ -15,7 +15,35 @@ namespace WebApplication1.Controllers
         {
             _config = config;
         }
-        //get store procedure   
+
+        [HttpPut("UpdateUser")]
+        public IEnumerable<UserModel> UpdateUser(string username, string password, int id)
+        {
+            List<UserModel> users = new List<UserModel>();
+            string dbconnection = _config.GetConnectionString("DefaultConnection");
+            using (SqlConnection con = new SqlConnection(dbconnection))
+            {
+                using (SqlCommand cmd = new SqlCommand("spUpdateUser", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@username", username);
+                    cmd.Parameters.AddWithValue("@userpassword", password);
+                    cmd.Parameters.AddWithValue("@userid", id);
+                    con.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        UserModel user = new UserModel();
+                        user.Id = Convert.ToInt32(rdr["Id"]);
+                        user.UserName = rdr["UserName"].ToString();
+                        user.UserPassword = rdr["UserPassword"].ToString();
+                        users.Add(user);
+                    }
+                }
+            }
+            return users;
+        }
+
         [HttpPost("user")]
         public IEnumerable<UserModel> PostUser(string username, string password)
         {
