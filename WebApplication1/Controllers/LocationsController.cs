8432958128c96cebf31c953b3944ae7034094df8
datboiluiskrrt <datboiluiskrrt@gmail.com>
2023-02-16 00:00:20 -0600

    Able to get locations by city id. Need to fix issue with aptnumber being null.

---
+++ WebApplication1/Controllers/LocationsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationsController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public LocationsController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet("GetLocationsByCityId")]
+        public IEnumerable<LocationsModel> UpdateUser(int Id)
+        {
+            List<LocationsModel> Locations = new List<LocationsModel>();
+            string dbconnection = _config.GetConnectionString("DefaultConnection");
+            using (SqlConnection con = new SqlConnection(dbconnection))
+            {
+                using (SqlCommand cmd = new SqlCommand("getAllLocationsByCityId", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Id", Id);
+                    con.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        LocationsModel Location = new LocationsModel();
+                        Location.Id = Convert.ToInt32(rdr["Id"]);
+                        Location.Active = rdr["Active"].ToString();
+                        //Location.AptNumber = rdr["AptNumber"] != null ? Convert.ToInt32(rdr["AptNumber"]) : 0;
+                        Location.StreetName = rdr["StreetName"].ToString();
+                        Location.StateId = Convert.ToInt32(rdr["StateId"]);
+                        Location.UserId = Convert.ToInt32(rdr["UserId"]);
+                        Location.CityId = Convert.ToInt32(rdr["CityId"]);
+                        Location.CountryId = Convert.ToInt32(rdr["CountryId"]);
+                        Location.HouseNumber = Convert.ToInt32(rdr["HouseNumber"]);
+                        Locations.Add(Location);
+                    }
+                }
+            }
+            return Locations;
+        }
+
+    }
+}
