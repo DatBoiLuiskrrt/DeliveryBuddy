2aaf40231644bf9aced84d5585d3ec8d160887e6
datboiluiskrrt <datboiluiskrrt@gmail.com>
2023-02-15 19:52:55 -0600

    Created delete endpoint for users.

--- WebApplication1/Controllers/UsersController.cs
+++ WebApplication1/Controllers/UsersController.cs
@@ -71,5 +71,33 @@ namespace WebApplication1.Controllers
             }
             return users;
         }
+
+        [HttpDelete("DeleteUser")]
+        public IEnumerable<UserModel> DeleteUser(int id)
+        {
+            List<UserModel> users = new List<UserModel>();
+            string dbconnection = _config.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection con = new SqlConnection(dbconnection))
+            {
+                using (SqlCommand cmd = new SqlCommand("spDeleteUser", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@userid", id);
+                    con.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        UserModel user = new UserModel();
+                        user.Id = Convert.ToInt32(rdr["Id"]);
+                        user.UserName = rdr["UserName"].ToString();
+                        user.UserPassword = rdr["UserPassword"].ToString();
+                        users.Add(user);
+                    }
+                }
+            }
+            return users;
+        }
+
     }
 }
