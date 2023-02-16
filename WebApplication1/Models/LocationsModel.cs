7572aa60353a1aa9d69d787387c1ada4da82ddac
datboiluiskrrt <datboiluiskrrt@gmail.com>
2023-02-15 23:23:04 -0600

    Created Locations model

---
+++ WebApplication1/Models/LocationsModel.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplication1.Models
+{
+    public class LocationsModel
+    {
+        public int Id { get; set; }
+        public string StreetName { get; set; } = string.Empty;
+        public int HouseNumber { get; set; } = 0;
+        public int? AptNumber { get; set; }
+        public string? Active { get; set; }
+        public int CityId { get; set; }
+        public int CountryId { get; set; }
+        public int UserId { get; set; }
+        public int StateId { get; set; }
+
+
+
+    }
+}
