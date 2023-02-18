783b94a8ae5c3edf0a97307ef69bdf21d3e4d3b1
datboiluiskrrt <datboiluiskrrt@gmail.com>
2023-02-18 14:53:03 -0600

    Able to get all locations by city id.

---
+++ DeliveryBuddyFE/Models/LocationsModel.cs
@@ -0,0 +1,14 @@
+﻿namespace DeliveryBuddyFE.Models;
+
+public class LocationsModel
+{
+    public int Id { get; set; }
+    public string? StreetName { get; set; } = string.Empty;
+    public int HouseNumber { get; set; } = 0;
+    public int? AptNumber { get; set; }
+    public string? Active { get; set; }
+    public int CityId { get; set; }
+    public int CountryId { get; set; }
+    public int UserId { get; set; }
+    public int StateId { get; set; }
+}
