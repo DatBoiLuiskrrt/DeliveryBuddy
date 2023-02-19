49bec650f21cb88a2040283b65e0d1e2db50879d
datboiluiskrrt <datboiluiskrrt@gmail.com>
2023-02-19 13:34:11 -0600

    Able to get all cities by ID API

---
+++ WebApplication1/Controllers/CityController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class CityController : Controller
+    {
+        private readonly IConfiguration _config;
+        public CityController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet("GetAllCitiesByStateId")]
+        public IEnumerable<CityModel> GetCities(int Id)
+        {
+            List<CityModel> Cities = new List<CityModel>();
+            string dbconnection = _config.GetConnectionString("DefaultConnection");
+            using (SqlConnection con = new SqlConnection(dbconnection))
+            {
+                using (SqlCommand cmd = new SqlCommand("spGetAllCitiesByStateId", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Id", Id);
+                    con.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        CityModel City = new CityModel();
+                        while (rdr.Read())
+                        {
+                            City.Id = Convert.ToInt32(rdr["Id"]);
+                            City.CityName = rdr["CityName"].ToString();
+                            City.StateId = Convert.ToInt32(rdr["StateId"]);
+                            Cities.Add(City);
+                        }
+                    }
+                }
+            }
+            return Cities;
+        }
+    }
+}
---
+++ WebApplication1/Models/CityModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models;
+
+public class CityModel
+{
+    public int Id { get; set; }
+    public string? CityName { get; set; }
+    public int StateId { get; set; }
+
+}
