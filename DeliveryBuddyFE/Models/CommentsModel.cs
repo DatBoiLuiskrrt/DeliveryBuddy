1ef2d3d0785497eb5fbb0a1406e6cdf799d10e86
datboiluiskrrt <datboiluiskrrt@gmail.com>
2023-02-22 18:04:06 -0600

    Able to display comments from locations.

---
+++ DeliveryBuddyFE/Models/CommentsModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DeliveryBuddyFE.Models
+{
+    public class CommentsModel
+    {
+        public int Id { get; set; }
+        public string? CommentDescription { get; set; }
+        public int? UserId { get; set; }
+        public int? LocationId { get; set; }
+    }
+}
